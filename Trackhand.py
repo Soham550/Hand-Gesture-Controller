1b5d73cd03ce6112c146ac7d13dc7cb133d667cd
Soham <145895060+Soham550@users.noreply.github.com>
2024-10-30 22:39:00 +0530

    Trackhand.py
    Track the hand  landmarks

--- Trackhand.py
+++ Trackhand.py
@@ -5,7 +5,7 @@ import mediapipe as mp
 class HandTracker:
     def __init__(self):
         self.mphands = mp.solutions.hands
-        self.hands = self.mphands.Hands()
+        self.hands = self.mphands.Hands(max_num_hands=1)
         self.mpDrawing = mp.solutions.drawing_utils
 
     def track_hands(self, frame):
@@ -17,4 +17,5 @@ class HandTracker:
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 self.mpDrawing.draw_landmarks(frame, hand_landmarks, self.mphands.HAND_CONNECTIONS)
+                break  
         return frame
